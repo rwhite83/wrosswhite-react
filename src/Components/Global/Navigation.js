50eb6a7641ced438549241d3d7389288be6dc047
rwhite83 <rwhite83@hotmail.com>
2020-02-18 16:15:16 -0800

    navbar detoggle not working

--- src/Components/Global/Navigation.js
+++ src/Components/Global/Navigation.js
@@ -8,36 +8,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class Navigation extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            expanded: false
+        };
+        this.toggleExpanded = this.toggleExpanded.bind(this);
+    }
+
+    toggleExpanded() {
+            this.state.expanded ? 
+                this.setState({ expanded: false }) :
+                this.setState({ expanded: true })
+    }
+
     render() {
         return (
-            <Navbar className="sticky-top navbar navbar-expand-lg navbar-dark bg-primary" id="sideNav" collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar expanded={this.state.expanded} className="sticky-top navbar navbar-expand-lg navbar-dark bg-primary" id="sideNav" collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Navbar.Brand href="#home">W. Ross White</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Toggle onClick={() => this.toggleExpanded() } aria-controls="responsive-navbar-nav" />
                 <a className="navbar-brand js-scroll-trigger" href="#about" id="nav-image">
                     <span className="d-none d-lg-block">
                         <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt=""></img>
                     </span>
                 </a>
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link js-scroll-trigger" href="#page-top">About</a>
+                    <Nav expanded="false" className="mr-auto">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#page-top" onClick={this.toggleExpanded}>About</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#experience">Experience</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#education">Education</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#skills">Skills</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#interests">Interests</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#awards">Awards</a>
                             </li>
                         </ul>
                     </Nav>
